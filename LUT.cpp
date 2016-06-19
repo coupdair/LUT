a03f3696206c3c43df7d2c42a498ccf719e0c16a
Sebastien COUDERT <coupdair@gmail.com>
2016-06-19 14:40:57 +0200

    LUT: 123 node grid.

--- LUT.cpp
+++ LUT.cpp
@@ -8,6 +8,11 @@ int main()
   const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
   cimg_forXY(image,x,y)
     image(x,y)=123;
-  image.display("123");
+  int stepx=3;
+  int stepy=3;
+  for (int y=0; y<(int)(image._height); y+=stepy)
+    for (int x=0; x<(int)(image._width); x+=stepx)
+      image(x,y)=0;
+  image.display("node grid");
   return 0;
 }
