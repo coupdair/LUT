e4cef7a546f8135a86826c44faa7dde6fb517db9
Sebastien COUDERT <coupdair@gmail.com>
2016-06-19 21:00:35 +0200

    LUT: black nodes w RB lines.

--- LUT.cpp
+++ LUT.cpp
@@ -4,15 +4,17 @@ using namespace cimg_library;
 
 int main()
 {
-  CImg<unsigned char> image(512,512);
-  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-  cimg_forXY(image,x,y)
-    image(x,y)=123;
-  int stepx=3;
-  int stepy=3;
-  for (int y=0; y<(int)(image._height); y+=stepy)
-    for (int x=0; x<(int)(image._width); x+=stepx)
-      image(x,y)=0;
+  CImg<unsigned char> image(1024,512,1,3);
+  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 }, black[] = { 0,0,0 };
+  image.fill(0);
+  int stepx=4;
+  int stepy=4;
+  //draw horizontal lines
+  for(int y=0; y<(int)(image.height()); y+=stepy)
+    image.draw_line(0,y,image.width()-1,y,blue);
+  //draw vertical lines
+  for(int x=0; x<(int)(image.width()); x+=stepx)
+    image.draw_line(x,0,x,image.height()-1,red);
   image.display("node grid");
   return 0;
 }
