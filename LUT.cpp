ddf150d3784c22d0271fce28d7a6bd11057fef35
Sebastien COUDERT <coupdair@gmail.com>
2016-06-19 22:43:35 +0200

    LUT: add partition colors.

--- LUT.cpp
+++ LUT.cpp
@@ -8,12 +8,23 @@ int main()
   int height=128;
   int zoom=4;int step=zoom;
   CImg<unsigned char> image(width,height,1,3);
-  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 }, black[] = { 0,0,0 }, white[] = { 222,222,222 };
+  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 },
+    cP128[] = { 64,64,64 }, cP64[] = { 128,128,128 }, cP32[] = { 196,196,196 }, cP16[] = { 222,222,222 }, cP8[] = { 242,242,242 },
+    black[] = { 0,0,0 }, white[] = { 222,222,222 };
   //partition color
+  {
   image.fill(255);//Pall
-  image.draw_rectangle(0,0,127,height,red);//P128
-  image.draw_rectangle(128,0,128+64,height,blue);//P64
-  image.draw_rectangle(128+64+1,0,128+64+1+32,height,white);//P64
+  int i=0,j=127;
+  image.draw_rectangle(i,0,j,height,cP128);
+  i=j+1;j+=64;
+  image.draw_rectangle(i,0,j,height,cP64);
+  i=j+1;j+=32;
+  image.draw_rectangle(i,0,j,height,cP32);
+  i=j+1;j+=16;
+  image.draw_rectangle(i,0,j,height,cP16);
+  i=j+1;j+=8;
+  image.draw_rectangle(i,0,j,height,cP8);
+  }
   //partition sum
   image.draw_line(0,0,image.width()-1,0,black);
   image.draw_line(0,0,0,image.height()-1,black);
@@ -32,8 +43,8 @@ int main()
     int i=ti(n);
     int j=tj(n);
     image.draw_point(i,j,green);//IP
-    if(image(i,0,0,0)==black[0]&&image(i,0,0,1)==black[1]&&image(i,0,0,2)==black[2]) image.draw_point(i,0,green); else image.draw_point(i,0,red);//sum
-    if(image(0,j,0,0)==black[0]&&image(0,j,0,1)==black[1]&&image(0,j,0,2)==black[2]) image.draw_point(0,j,green); else image.draw_point(0,j,red);//sum
+    if(image(i,0,0,0)==black[0]&&image(i,0,0,1)==black[1]&&image(i,0,0,2)==black[2]) image.draw_point(i,0,green); else {image.draw_point(i,0,red);image.draw_point(0,0,red);}//sum
+    if(image(0,j,0,0)==black[0]&&image(0,j,0,1)==black[1]&&image(0,j,0,2)==black[2]) image.draw_point(0,j,green); else {image.draw_point(0,j,red);image.draw_point(0,0,red);}//sum
   }
   //zoom
   image.resize(-zoom*100,-zoom*100);
