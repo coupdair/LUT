e3eb7964f26bf2af9c108906894e4dd0e72c4845
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2016-06-20 11:56:39 +0200

    GUI: value as fourth position in spectrum.

--- claLUT.cpp
+++ claLUT.cpp
@@ -7,10 +7,10 @@ int main()
   int width=256;
   int height=128;
   int zoom=6;int step=zoom;
-  CImg<unsigned char> image(width,height,1,3);
-  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 },
-    cP128[] = { 64,64,64 }, cP64[] = { 128,128,128 }, cP32[] = { 196,196,196 }, cP16[] = { 222,222,222 }, cP8[] = { 242,242,242 },
-    black[] = { 0,0,0 }, white[] = { 222,222,222 };
+  CImg<unsigned char> image(width,height,1,4);
+  const unsigned char red[] = { 255,0,0,255 }, green[] = { 0,255,0,255 }, blue[] = { 0,0,255,255 },
+    cP128[] = { 64,64,64,255 }, cP64[] = { 128,128,128,255 }, cP32[] = { 196,196,196,255 }, cP16[] = { 222,222,222,255 }, cP8[] = { 242,242,242,255 },
+    black[] = { 0,0,0,255 }, white[] = { 222,222,222,255 };
   //partition color
   {
   image.fill(255);//Pall
@@ -42,9 +42,11 @@ int main()
   {
     int i=ti(n);
     int j=tj(n);
-    image.draw_point(i,j,green);//IP
-    if(image(i,0,0,0)==black[0]&&image(i,0,0,1)==black[1]&&image(i,0,0,2)==black[2]) image.draw_point(i,0,green); else {image.draw_point(i,0,red);image.draw_point(0,0,red);}//sum
-    if(image(0,j,0,0)==black[0]&&image(0,j,0,1)==black[1]&&image(0,j,0,2)==black[2]) image.draw_point(0,j,green); else {image.draw_point(0,j,red);image.draw_point(0,0,red);}//sum
+    CImg<unsigned char>  color(1,1,1,4);color.draw_point(0,0,green);
+    color(3)=j;
+    image.draw_point(i,j,color.data());//IP
+    if(image(i,0,0,0)==black[0]&&image(i,0,0,1)==black[1]&&image(i,0,0,2)==black[2]) image.draw_point(i,0,color.data()); else {image.draw_point(i,0,red);image.draw_point(0,0,red);}//sum
+    if(image(0,j,0,0)==black[0]&&image(0,j,0,1)==black[1]&&image(0,j,0,2)==black[2]) image.draw_point(0,j,color.data()); else {image.draw_point(0,j,red);image.draw_point(0,0,red);}//sum
   }
   //GUI
   //set other point(s) by mouse
@@ -66,9 +68,11 @@ int main()
       continue;
     }
     //else add point
-    image.draw_point(i,j,green);
-    if(image(i,0,0,0)==black[0]&&image(i,0,0,1)==black[1]&&image(i,0,0,2)==black[2]) image.draw_point(i,0,green); else {image.draw_point(i,0,red);image.draw_point(0,0,red);}//sum
-    if(image(0,j,0,0)==black[0]&&image(0,j,0,1)==black[1]&&image(0,j,0,2)==black[2]) image.draw_point(0,j,green); else {image.draw_point(0,j,red);image.draw_point(0,0,red);}//sum
+    unsigned char color[4];strcpy((char*)color,(char*)green);
+    color[3]=j;
+    image.draw_point(i,j,color);
+    if(image(i,0,0,0)==black[0]&&image(i,0,0,1)==black[1]&&image(i,0,0,2)==black[2]) image.draw_point(i,0,color); else {image.draw_point(i,0,red);image.draw_point(0,0,red);}//sum
+    if(image(0,j,0,0)==black[0]&&image(0,j,0,1)==black[1]&&image(0,j,0,2)==black[2]) image.draw_point(0,j,color); else {image.draw_point(0,j,red);image.draw_point(0,0,red);}//sum
   }//point selection by hand
   disp.close();
   //zoom
