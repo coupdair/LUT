a8102988f20b199fd70343a82100015f2bf9720c
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2019-08-07 12:24:01 +0200

    temp: no this in twice inheritance

--- this.cpp
+++ this.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 class Polygon {
-  protected:
-    int width, height;
   public:
+    int width, height;
     void set_values (int a, int b)
       { width=a; height=b;}
  };
@@ -16,6 +15,12 @@ class Rectangle: public Polygon {
       { return width * height; }
  };
 
+class Square: public Rectangle {
+  public:
+    int area ()
+      { return width * width; }
+ };
+
 class Triangle: public Polygon {
   public:
     int area ()
@@ -24,10 +29,14 @@ class Triangle: public Polygon {
   
 int main () {
   Rectangle rect;
+  Square square;
   Triangle trgl;
   rect.set_values (4,5);
+  square.set_values (4,0);
   trgl.set_values (4,5);
   cout << rect.area() << '\n';
+  cout << square.area() << '\n';
   cout << trgl.area() << '\n';
   return 0;
-}
+}//main
+
