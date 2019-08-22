dd04072fbcc9c7273fcc9579adb5a5130b03a12a
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2019-08-22 11:46:41 +0200

    seq: setup sequential process (v0.3.1d)

--- Makefile
+++ Makefile
@@ -15,12 +15,13 @@ LIB_BOOST_ASIO=-lboost_system
 LIB_BOOST_COMPUTE=-lMali -L/usr/lib/aarch64-linux-gnu/ -DBOOST_COMPUTE_MAX_CL_VERSION=102
 
 DO_GPU=-DDO_GPU $(LIB_BOOST_COMPUTE)
-#DO_GPU=
+DO_GPU=
 
 #source package
 SRC_DATA_BUFFER=thread_lock.hpp CDataAccess.hpp CDataBuffer.hpp
 
-all: process send receive
+#all: process send receive
+all: process_sequential
 
 gui: main.cpp
 	g++ -O0 -o generate.X main.cpp -I../CImg -Wall -W -ansi -pedantic -Dcimg_use_vt100 -lpthread -lm -fopenmp -lboost_system $(LIB_XWINDOWS) && ./generate.X -h -I && ./generate.X -v > VERSION
@@ -29,6 +30,10 @@ gui: main.cpp
 process: process.cpp $(SRC_DATA_BUFFER) CDataGenerator.hpp CDataProcessor.hpp CDataProcessorGPU.hpp CDataStore.hpp
 	g++ -O0 -o process   process.cpp $(LIB_CIMG) -Dcimg_display=0 $(DO_GPU) && ./process -h -I && ./process -v > VERSION
 	./process -h 2> process_help.output
+
+process_sequential: process_sequential.cpp $(SRC_DATA_BUFFER) CDataGenerator.hpp CDataProcessor.hpp CDataProcessorGPU.hpp CDataStore.hpp
+	g++ -O0 -o process_sequential   process_sequential.cpp $(LIB_CIMG) -Dcimg_display=0 $(DO_GPU) && ./process_sequential -h -I && ./process_sequential -v > VERSION
+	./process_sequential -h 2> process_sequential_help.output
 
 send: send.cpp $(SRC_DATA_BUFFER) CDataGenerator.hpp CDataSend.hpp
 	g++ -O0 -o send   send.cpp  $(LIB_CIMG) $(LIB_BOOST_ASIO) -Dcimg_display=0 && ./send -h -I && ./send -v > VERSION
@@ -39,7 +44,10 @@ receive: receive.cpp $(SRC_DATA_BUFFER) CDataReceive.hpp CDataProcessor.hpp CDat
 	./receive -h 2> receive_help.output
 
 process_run:
-	./process -c 3 -s $(FRAME_SIZE)
+	./process -c 3 -s $(FRAME_SIZE) -o $(DATA)$(DIN)$(FIN) -r $(DATA)$(DOUT)$(FOUT)
+
+process_sequential_run:
+	./process_sequential -s $(FRAME_SIZE) -o $(DATA)$(DIN)$(FIN) -r $(DATA)$(DOUT)$(FOUT)
 
 send_run:
 	./send    -c 2 -s $(FRAME_SIZE) -b  8 -n 256 -w 65432109
---
+++ process_sequential.cpp
@@ -0,0 +1,171 @@
+//CoolImage
+#include "CImg.h"
+
+//C++ base
+#include <iostream>
+#include <string>
+#include <vector>
+
+//OpenMP
+#include <omp.h>
+
+#define VERSION "v0.3.1d"
+
+//thread lock
+#include "CDataGenerator.hpp"
+#include "CDataProcessor_morphomath.hpp"
+#ifdef DO_GPU
+#include "CDataProcessorGPU.hpp"
+#endif
+#include "CDataStore.hpp"
+
+using namespace cimg_library;
+
+#define S 0 //sample
+
+//types
+typedef unsigned char Taccess;
+typedef unsigned int  Tdata;
+
+int main(int argc,char **argv)
+{
+  ///command arguments, i.e. CLI option
+  cimg_usage(std::string("generate, process and store data.\n" \
+  " It uses different GNU libraries (see --info option)\n\n" \
+  " usage: ./process -h\n" \
+  "        ./process -s 1024 -n 123 -X true -p 1234 -i 10.10.15.1 -w 1234657\n" \
+  "\n version: "+std::string(VERSION)+"\n compilation date:" \
+  ).c_str());//cimg_usage
+
+  const char* imagefilename = cimg_option("-o","sample.cimg","output file name (e.g. \"-o data.cimg -d 3\" gives data_???.cimg)");
+  const int digit=cimg_option("-d",6,  "number of digit for file names");
+  const int width=cimg_option("-s",1024, "size   of udp buffer");
+  const int count=cimg_option("-n",256,  "number of frames");
+  const int nbuffer=1;
+#ifdef DO_GPU
+  const bool use_GPU_G=cimg_option("-G",false,NULL);//-G hidden option
+        bool use_GPU=cimg_option("--use-GPU",use_GPU_G,"show GUI (or -G option)");use_GPU=use_GPU_G|use_GPU;//same --use-GPU or -G option
+#endif //DO_GPU
+
+  ///standard options
+  #if cimg_display!=0
+  const bool show_X=cimg_option("-X",true,NULL);//-X hidden option
+  bool show=cimg_option("--show",show_X,"show GUI (or -X option)");show=show_X|show;//same --show or -X option
+  #endif
+  const bool show_h   =cimg_option("-h",    false,NULL);//-h hidden option
+        bool show_help=cimg_option("--help",show_h,"help (or -h option)");show_help=show_h|show_help; //same --help or -h option
+  bool show_info=cimg_option("-I",false,NULL);//-I hidden option
+  if( cimg_option("--info",show_info,"show compilation options (or -I option)") ) {show_info=true;cimg_library::cimg::info();}//same --info or -I option
+  bool show_version=cimg_option("-v",false,NULL);//-v hidden option
+  if( cimg_option("--version",show_version,"show version (or -v option)") ) {show_version=true;std::cout<<VERSION<<std::endl;return 0;}//same --version or -v option
+  if(show_help) {/*print_help(std::cerr);*/return 0;}
+  //}CLI option
+
+  //OpenMP
+  {//user number of thread
+    omp_set_dynamic(0);
+    omp_set_num_threads(1);
+  }//user
+
+  //OpenMP locks
+  omp_lock_t print_lock;omp_init_lock(&print_lock);
+
+  //! circular buffer
+  CImgList<Tdata> images(nbuffer,width,1,1,1);
+  images[0].fill(0);
+  images[0].print("image",false);
+  //access locking
+  omp_lock_t lck;omp_init_lock(&lck);
+
+  //! result circular buffer
+  CImgList<Tdata> results(nbuffer,width,1,1,1);
+  results[0].fill(0);
+  results[0].print("result",false);
+  //accessR locking
+  omp_lock_t lckR;omp_init_lock(&lckR);
+
+  //! access and status of buffer
+  CImg<Taccess> access(nbuffer,1,1,1);
+  access.fill(0);//free
+  access.print("access (free state)",false);fflush(stderr);
+
+  //! access and status of Result buffer
+  CImg<Taccess> accessR(nbuffer,1,1,1);
+  accessR.fill(0);//free
+  accessR.print("accessR (free state)",false);fflush(stderr);
+
+  //! thread locks
+  std::vector<omp_lock_t*> locks;locks.push_back(&print_lock);locks.push_back(&lck);locks.push_back(&lckR);
+  std::vector<omp_lock_t*> locksR;locksR.push_back(&print_lock);locksR.push_back(&lckR);
+
+#ifdef DO_GPU
+  //Choosing the target for OpenCL computing
+  boost::compute::device gpu = boost::compute::system::default_device();
+  #pragma omp parallel shared(print_lock, access,images, accessR,results, gpu)
+#else
+  #pragma omp parallel shared(print_lock, access,images, accessR,results)
+#endif //!DO_GPU
+  {
+  int id=omp_get_thread_num(),tn=omp_get_num_threads();
+
+  #pragma omp single
+  {
+  printf("\ninfo: running single thread over %d.\n",tn);fflush(stdout);
+  }//single
+
+  //run threads
+  switch(id)
+  {
+    case 0:
+    {//sequential
+     //generate
+      CDataGenerator<Tdata,Taccess> generate(locks);
+     //process
+/*
+#ifdef DO_GPU
+      if(use_GPU)
+      {//GPU
+      std::cout<<"information: use GPU for processing."<<std::endl<<std::flush;
+      CDataProcessorGPU<Tdata, Taccess> process(locks, gpu,width
+      , CDataAccess::STATUS_FILLED, CDataAccess::STATUS_FREE  //images
+      , CDataAccess::STATUS_FREE,   CDataAccess::STATUS_FILLED//results
+      );
+      process.run(access,images, accessR,results, count);
+      }//GPU
+      else
+#endif
+      {//CPU
+*/
+      std::cout<<"information: use CPU for processing."<<std::endl<<std::flush;
+      CDataProcessor<Tdata,Taccess> process(locks
+      , CDataAccess::STATUS_FILLED, CDataAccess::STATUS_FREE  //images
+      , CDataAccess::STATUS_FREE,   CDataAccess::STATUS_FILLED//results
+      );
+//      }//CPU
+     //store
+      CDataStore<Tdata,Taccess> store(locksR, imagefilename,digit, CDataAccess::STATUS_FILLED);
+      //run
+      /*
+      generate.run(access,images, count);
+      process.run(access,images, accessR,results, count);
+      store.run(accessR,results, count);
+      */
+      for(unsigned int i=0;i<count;++i)
+      {
+        generate.iteration(access,images,0,i);
+        process.iteration(access,images, accessR,results, 0,i);
+        store.iteration(accessR,results, 0,i);
+      }//vector loop
+      break;
+    }//sequential
+  }//switch(id)
+  }//parallel section
+
+  access.print("access (free state)",false);fflush(stderr);
+  images.print("CImgList",false);
+
+  access.print("accessR (free state)",false);fflush(stderr);
+  images.print("CImgListR",false);
+  return 0;
+}//main
+
