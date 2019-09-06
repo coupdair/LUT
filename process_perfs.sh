ccc909bec2e5ae92bef4c84972827e469a741236
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2019-09-06 14:38:18 +0200

    perf: 4096BoF 1 to 16 CPU process threads (v0.3.7)

--- process_perfs.sh
+++ process_perfs.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-fn=123456
+#fn=123456
+fnpc=123456
+bnpc=8
 gpu=--use-GPU
 gpu=
 
 #run program for several frame sizes
-for i in `cat frames.txt`
+#for fs in `cat frames.txt`
+fs=4096
+for pn in 1 2 4 8 16
 do
   make clear
-  echo frame $i
-  time ./process -s $i $gpu -n $fn -o /media/temp/samples/sample.cimg -r /media/temp/results/sample.cimg &>/dev/null
-  du -shc /media/temp/samples/
-done 2>&1 | grep -e frame -e real -e total > process_perfs.txt
+  ((tn=pn+2))
+  ((fn=pn*fnpc))
+  ((bn=pn*bnpc))
+  echo frame $fs
+  echo processing thread $pn
+  time ./process -s $fs -c $tn -b $bn $gpu -n $fn -o /media/temp/samples/sample.cimg -r /media/temp/results/sample.cimg &>/dev/null #real: elapsed time
+  du -shc /media/temp/samples/ #total: stored data size
+done 2>&1 | grep -e frame -e thread -e real -e total | tee process_perfs.txt
 
 #lines to one frame per line
-for i in `cat frames.txt`
+#for i in `cat frames.txt`
+for pn in 1 2 4 8 16
 do
-  s=`grep $i -A 2 process_perfs.txt`
+  s=`grep "thread $pn" -B 1 -A 2 process_perfs.txt`
   f=`echo $s | cut -d' ' -f2`
-  t=`echo $s | cut -d' ' -f4 | sed 's/m/min+/;s/\./s/' | head -c -5`
-  g=`echo $s | cut -d' ' -f5`'B'
+  p=`echo $s | cut -d' ' -f5`
+  t=`echo $s | cut -d' ' -f7 | sed 's/m/min+/;s/\./s/' | head -c -5`
+  g=`echo $s | cut -d' ' -f8`'B'
   r=`units "$g/($t)" MB/s -t | cut -d'.' -f1`
   ts=`units "$t" s -t | cut -d'.' -f1`
-  echo $fn $f'B' $t $ts's' $g $r'MB/s'
+  echo $pn'pc' $bnpc'cbpc' $f'BoF' $t $ts's' $g $r'MB/s'
 done > process_perfs.dat
 
